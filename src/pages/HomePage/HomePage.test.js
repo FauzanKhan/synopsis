213ba67aba3514a965a6e3721ebd0a9bb8764f23
FauzanKhan <fauzankhan93@gmail.com>
2019-05-26 22:06:30 +0200

    Added more tests

---
+++ src/pages/HomePage/HomePage.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, cleanup, act } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import { ThemeProvider } from '../../components/ThemeProvider/ThemeProvider';
+import { StoreContext } from '../../StoreContext';
+import { HomePage } from './HomePage';
+
+jest.mock('../../StoreContext', () => {
+  const { createContext } = require('react');
+  return { StoreContext: createContext() };
+});
+
+jest.mock('../../shared/api', () => ({ api: { get: () => Promise.resolve({}) } }));
+
+const props = {
+  history: {
+    push: jest.fn(),
+  },
+};
+
+describe('HomePage', () => {
+  afterEach(cleanup);
+
+  it('renders login page/login button when user is not loggedIn', () => {
+    act(() => {
+      const { getByTestId } = render(
+        <StoreContext.Provider value={{ userIsLoggedIn: false }}>
+          <ThemeProvider value={{ theme: { colors: {} } }}>
+            <HomePage {...props} />
+          </ThemeProvider>
+        </StoreContext.Provider>,
+      );
+
+      expect(getByTestId('login-button')).toBeDefined();
+    });
+  });
+
+  it("should redirect user to discovery page when (s)he' logged in", () => {
+    act(() => {
+      render(
+        <StoreContext.Provider value={{ userIsLoggedIn: true }}>
+          <ThemeProvider value={{ theme: { fontSize: {} } }}>
+            <HomePage {...props} />
+          </ThemeProvider>
+        </StoreContext.Provider>,
+      );
+
+      expect(props.history.push).toBeCalledWith('/discover');
+    });
+  });
+});
